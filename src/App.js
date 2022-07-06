c355eeeccabaab82fd41177981b2e11b419d6e7c
Odelon Jhey Pacalso <odjhey@gmail.com>
2022-07-06 17:38:24 +0800

    rewrite container to not use global.document

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import Nav from './components/Nav';
-import Container from './components/Container';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import Nav from "./components/Nav";
+import Container from "./components/Container";
+import Container2 from "./components/Container2";
 
 function App() {
   return (
-    <div className="App"><Nav />
+    <div className="App">
+      <Nav />
       <header className="App-header">
-        <Container />
+        {/*<Container />*/}
+        <Container2 />
       </header>
     </div>
   );
---
+++ src/components/Container2.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import { data } from "./data";
+
+const Container = () => {
+  const [cities, setCities] = useState(data);
+  const [filteredCities, setFilteredCities] = useState(data);
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    if (searchTerm) {
+      const filteredCities = cities.filter((city) =>
+        `${city.city} ${city.state}`
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase())
+      );
+      setFilteredCities(filteredCities);
+    } else {
+      setFilteredCities([]);
+    }
+  }, [searchTerm, cities]);
+
+  return (
+    <div className="container-search">
+      <input
+        type="text"
+        className="search"
+        placeholder="City or State"
+        value={searchTerm}
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+
+      {searchTerm && filteredCities.length > 0 ? (
+        <span id="count">
+          {filteredCities.length} cities found for "<b>{searchTerm}</b>"
+        </span>
+      ) : (
+        <>
+          <span id="count">(↑) Type here to check cities (↑)</span>
+          <span id="count">Filter for a city or a state</span>
+        </>
+      )}
+
+      <ul className="suggestions">
+        {filteredCities.map((city) => (
+          <li key={city.rank}>
+            <span className="name">
+              <TextWithHighlight
+                term={searchTerm}
+                text={`${city.city}, ${city.state}`}
+              />
+            </span>{" "}
+            <span className="poulation">{city.population}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const TextWithHighlight = ({ text = "", term = "" }) => {
+  if (!term.trim()) {
+    return <span>{text}</span>;
+  }
+  const regex = new RegExp(`(${term})`, "gi");
+  const parts = text.split(regex);
+
+  return (
+    <span>
+      {parts.filter(String).map((part, i) => {
+        return regex.test(part) ? (
+          <>
+            {/*<mark key={i}>{part}</mark>*/}
+            <span key={i} className="highlight">
+              {part}
+            </span>
+          </>
+        ) : (
+          <span key={i}>{part}</span>
+        );
+      })}
+    </span>
+  );
+};
+
+export default Container;
